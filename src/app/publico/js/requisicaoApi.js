c9e816bbb1b43200583ab8425f6234a11aa3ea5b
Fabinnreis <fabioreisti@outlook.com>
2020-09-23 11:44:13 -0300

    Remoção do método insere da classe requisicaoApi

--- src/app/publico/js/requisicaoApi.js
+++ src/app/publico/js/requisicaoApi.js
@@ -2,38 +2,6 @@ class RequisicaoApi{
     constructor(){
         throw 'Não precisa de instancia'
     }
-
-    static adicionaTarefa(id_form, event){
-        event.preventDefault();
-        const myHeaders = new Headers({'Content-type': 'application/json'});
-        const _form = document.getElementById(id_form);
-        
-        const _titulo = _form.form_title.value;
-        const _descricao = _form.form_description.value;
-        
-        const valores = {
-                            titulo: _titulo,
-                            descricao: _descricao,
-                        }
-
-        const estruturaReq = (req)=>{
-            return { method: 'POST',
-            headers: myHeaders,
-            mode: 'cors',
-            cache: 'default',
-            body: JSON.stringify(req) }
-        }
-        
-        fetch('http://127.0.0.1:3000/adiciona', estruturaReq(valores))
-        .then((response)=>{
-            console.log(response);
-            window.location.href = "/";
-        })
-        .catch(err=>err)
-
-        
-    }
-
     static deletaCard(event){
         const myHeaders = new Headers({'Content-type': 'application/json'});
         const id_tarefa = event.target.parentNode.dataset.idTarefa;
